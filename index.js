80ccc9481027f518521ffbd20fcac1e76d777bdf
TurjoSiam <t.siam24@gmail.com>
2025-02-22 00:43:47 +0600

    user related api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,49 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.k0g53.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // collections
+        const userCollection = client.db("TaskNimbus").collection("users");
+
+
+        // user related api
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get("/users", async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+
+
+
+
+    } finally {
+    }
+}
+run().catch(console.dir);
+z
 
 
 
