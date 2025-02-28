d9d5824c27df77129366694c65e55266669da3f6
TurjoSiam <t.siam24@gmail.com>
2025-02-28 11:01:00 +0600

    update and delete api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
@@ -13,16 +13,16 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k0g53.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k0g53.mongodb.net/TaskNimbus?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
     }
-});
+  });
 
 async function run() {
     try {
@@ -36,8 +36,8 @@ async function run() {
         // user related api
         app.post("/users", async (req, res) => {
             const user = req.body;
-            const existingUser = await userCollection.findOne({email: user.email})
-            if(existingUser){
+            const existingUser = await userCollection.findOne({ email: user.email })
+            if (existingUser) {
                 return res.send("User already exist")
             }
             const result = await userCollection.insertOne(user);
@@ -61,19 +61,42 @@ async function run() {
 
         app.get("/tasks/:email", async (req, res) => {
             const email = req.params.email;
-            const cursor = taskCollection.find({email: email})
+            const cursor = taskCollection.find({ email: email })
             const result = await cursor.toArray();
             res.json(result);
         });
 
-        // API to update tasks
-        app.put("/tasks", async (req, res) => {
-            const { taskId, category, position } = req.body;
-            await taskCollection.updateOne(
-                { _id: taskId },
-                { $set: { category, position } }
-            );
-            res.sendStatus(200);
+        // Update task category --->
+        app.patch("/updateCategory/:id", async (req, res) => {
+            const id = req.params.id;
+            const { category, timestamp } = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const updatedCategory = {
+                $set: {
+                    category: category,
+                },
+            };
+            const result = await taskCollection.updateOne(filter, updatedCategory);
+            res.send(result);
+        });
+
+        app.put("/updateTask/:id", async (req, res) => {
+            const task = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedCategory = {
+                $set: task,
+            };
+            const result = await taskCollection.updateOne(filter, updatedCategory);
+            res.send(result);
+        });
+
+        // Delete a task --->
+        app.delete("/deleteTask/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
         });
 
 
